d1c73ddfbd40fb7b42a82c94a92e9d715c5a4eb6
un.def <un.def@ya.ru>
2020-11-15 16:35:32 +0300

    Release version 0.1.0

--- src/dl_plus/extractors/un1def/goodgame.py
+++ src/dl_plus/extractors/un1def/goodgame.py
@@ -5,7 +5,7 @@ from dl_plus.extractor import Extractor, ExtractorError, ExtractorPlugin
 urljoin = ytdl.import_from('utils', 'urljoin')
 
 
-__version__ = '0.1.0.dev0'
+__version__ = '0.1.0'
 
 
 plugin = ExtractorPlugin(__name__)
