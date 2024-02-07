2a43ed7dea072d962c304899e199c5ea4a47d2a3
Dmitry Meyer <me@undef.im>
2024-02-07 14:01:30 +0000

    fix: address stream_id/stream_key mismatch

--- src/dl_plus/extractors/un1def/goodgame.py
+++ src/dl_plus/extractors/un1def/goodgame.py
@@ -36,39 +36,53 @@ class GoodGameBaseExtractor(Extractor):
             endpoint,
             item_id,
             query=query_args,
-            note=f'Downloading {description} metadata',
-            errnote=f'Unable to download {description} metadata',
+            note=f'Downloading {description} data',
+            errnote=f'Unable to download {description} data',
+        )
+
+    def _fetch_stream_by_stream_key(self, stream_key):
+        # stream_id and stream_key are not always the same, see:
+        # id = 5, key = "6" (Miker)
+        # id = 1644, key = "pomi" (Pomi)
+        # there is no API v4 method to fetch stream by key, we use
+        # legacy (v1) API to extract id first
+        player = self._fetch(
+            'https://goodgame.ru/api/player', src=stream_key,
+            item_id=stream_key, description='player',
+        )
+        stream_id = player['channel_id']
+        return self._fetch(
+            f'streams/2/id/{stream_id}',
+            item_id=stream_id, description='stream',
         )
 
 
 @plugin.register('stream')
 class GoodGameStreamExtractor(GoodGameBaseExtractor):
     DLP_REL_URL = (
-        r'(?:(?P<username>[^/?#]+)/?(?:#|$)|player\?(?P<stream_id>\d+))')
+        r'(?:(?P<username>[^/?#]+)/?(?:#|$)|player\?(?P<stream_key>[^&#]+))')
 
     _M3U8_URL_TEMPLATE = (
         'https://hls.goodgame.ru/manifest/{stream_key}_master.m3u8')
 
     def _real_extract(self, url):
-        username, stream_id = self._match_valid_url(url).group(
-            'username', 'stream_id')
+        username, stream_key = self._match_valid_url(url).group(
+            'username', 'stream_key')
         if username:
             stream = self._fetch(
                 f'streams/2/username/{username}',
                 item_id=username, description='stream',
             )
-            stream_id = stream.get('id')
-        else:
-            stream = self._fetch(
-                f'streams/2/id/{stream_id}',
-                item_id=stream_id, description='stream',
-            )
+            stream_key = stream['streamKey']
+        elif stream_key:
+            stream = self._fetch_stream_by_stream_key(stream_key)
             username = traverse_obj(stream, ('streamer', 'username'))
-        video_id = username or stream_id
+        else:
+            assert False, 'should not reach here'
+        video_id = username or stream_key
         if not stream.get('online', True):
             raise ExtractorError(f'{username} is offline', expected=True)
-        m3u8_url = self._M3U8_URL_TEMPLATE.format(
-            stream_key=stream['streamKey'])
+        m3u8_url = self._M3U8_URL_TEMPLATE.format(stream_key=stream_key)
         formats = self._extract_m3u8_formats(
             m3u8_url, video_id=video_id, fatal=True)
         return {
@@ -83,19 +97,19 @@ class GoodGameStreamExtractor(GoodGameBaseExtractor):
 
 @plugin.register('vod')
 class GoodGameVODExtractor(GoodGameBaseExtractor):
-    DLP_REL_URL = r'vods/(?P<stream_id>\d+)/(?P<timestamp>[0-9TZ:+-]+)'
+    DLP_REL_URL = r'vods/(?P<stream_key>[^/]+)/(?P<timestamp>[0-9TZ:+-]+)'
 
     _STORAGE_BASE_URL = 'https://storage2.goodgame.ru/'
     _THUMBNAIL_KEYS = ('jpgSmall', 'jpgFull', 'png')
 
     def _real_extract(self, url):
-        stream_id, timestamp = self._match_valid_url(url).group(
-            'stream_id', 'timestamp')
-        vod_surrogate_id = f'{stream_id}/{timestamp}'
+        stream_key, timestamp = self._match_valid_url(url).group(
+            'stream_key', 'timestamp')
+        video_id = f'{stream_key}/{timestamp}'
         vods = self._fetch(
             urljoin(self._STORAGE_BASE_URL, 'api/json/channel/video'),
-            streamId=stream_id,
-            item_id=stream_id, description='vods',
+            streamId=stream_key,
+            item_id=stream_key, description='vods',
         )
         for vod in vods['vods']:
             if vod['moddate'] == timestamp:
@@ -105,7 +119,7 @@ class GoodGameVODExtractor(GoodGameBaseExtractor):
         if m3u8_path := vod.get('m3u8path'):
             formats = self._extract_m3u8_formats(
                 self._build_absolute_url(m3u8_path),
-                video_id=vod_surrogate_id, fatal=False,
+                video_id=video_id, fatal=False,
             )
         else:
             formats = None
@@ -119,12 +133,9 @@ class GoodGameVODExtractor(GoodGameBaseExtractor):
                 'url': self._build_absolute_url(url),
                 'preference': preference,
             })
-        stream = self._fetch(
-            f'streams/2/id/{stream_id}',
-            item_id=stream_id, description='stream',
-        )
+        stream = self._fetch_stream_by_stream_key(stream_key)
         info_dict = {
-            'id': vod_surrogate_id,
+            'id': video_id,
             'title': timestamp,
             'creator': traverse_obj(stream, ('streamer', 'username')),
             'thumbnails': thumbnails,
